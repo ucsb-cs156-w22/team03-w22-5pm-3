2679af855f7ee3f3e89bcc7547c2dfe96b3f9138
qianyouwang <qianyou_wang@umail.ucsb.edu>
2022-02-16 13:35:45 -0800

    first commit for earthquakes

---
+++ src/main/java/edu/ucsb/cs156/example/collections/EarthquakeCollection.java
@@ -0,0 +1,13 @@
+package edu.ucsb.cs156.example.collections;
+
+import java.util.Optional;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+
+
+public class EarthquakeCollection {
+    
+}
