66966b0a89a75369fb58dd14cf88697486abe82e
qianyouwang <qianyou_wang@umail.ucsb.edu>
2022-02-16 14:54:11 -0800

    added earthquake collections with feature documents defined

--- src/main/java/edu/ucsb/cs156/example/collections/EarthquakeCollection.java
+++ src/main/java/edu/ucsb/cs156/example/collections/EarthquakeCollection.java
@@ -1,13 +1,12 @@
 package edu.ucsb.cs156.example.collections;
 
-import java.util.Optional;
-
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import edu.ucsb.cs156.example.documents.Feature;
 
+@Repository
+public interface EarthquakeCollection extends MongoRepository<Feature, ObjectId> {
 
-public class EarthquakeCollection {
-    
 }
