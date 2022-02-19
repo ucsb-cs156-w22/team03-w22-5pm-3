096eeddfe2631e9da7d1b5d0e8dcac3ddb714c7c
nicg6645 <41531696+nicg6645@users.noreply.github.com>
2022-02-19 14:51:23 -0800

    ng -- add form for collegiate subreddits

---
+++ frontend/src/fixtures/collegiateSubredditsFixtures.js
@@ -0,0 +1,30 @@
+const collegiateSubredditsFixtures = {
+  oneSubreddit: {
+    id: 1,
+    name: "UCSB",
+    locations: "Santa Barbara, CA",
+    subreddit: "UCSantaBarbara",
+  },
+  threeSubreddits: [
+    {
+      id: 1,
+      name: "UCSB",
+      locations: "Santa Barbara, CA",
+      subreddit: "UCSantaBarbara",
+    },
+    {
+      id: 1,
+      name: "UCLA",
+      locations: "Los Angeles, CA",
+      subreddit: "ucla",
+    },
+    {
+      id: 1,
+      name: "UC Berkeley",
+      locations: "Berkeley, CA",
+      subreddit: "berkeley",
+    },
+  ],
+};
+
+export { collegiateSubredditsFixtures };
