2292529fdd2d8b775bb154b980907b628f7be916
isaiahgarcia <isaiahgarcia@umail.ucsb.edu>
2022-02-20 12:22:29 -0800

    created storybook for earthquake

---
+++ frontend/src/stories/components/Earthquakes/EarthquakesTable.stories.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import EarthquakesTable from "main/components/Earthquakes/EarthquakesTable";
+import { earthquakeFixtures } from 'fixtures/earthquakeFixtures';
+
+export default {
+    title: 'components/earthquakes/EarthquakesTable',
+    component: Earthquakes
+};
+
+const Template = (args) => {
+    return (
+        <EarthquakesTable {...args} />
+    )
+};
+
+export const Empty = Template.bind({});
+
+Empty.args = {
+    earthquakes: []
+};
+
+export const ThreeEarthquakes = Template.bind({});
+
+ThreeEarthquakes.args = {
+    earthquakes: earthquakeFixtures.threeEarthquakes
+};
