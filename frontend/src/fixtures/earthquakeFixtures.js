03264190fbeab10d2db36435ade1058f784f4208
isaiahgarcia <isaiahgarcia@umail.ucsb.edu>
2022-02-20 12:04:59 -0800

    ig-added fixtures for earthquakes

---
+++ frontend/src/fixtures/earthquakeFixtures.js
@@ -0,0 +1,32 @@
+const earthquakeFixtures = {
+    oneEarthquake: {
+        "id": 1,
+        "title": "M 2.2 - 10km ESE of Ojai, CA",
+        "mag": 2.16,
+        "place": "10km ESE of Ojai, CA",
+        "time": 1644571919000 
+    },
+    threeEarthquakes: [
+        {
+            "id": 1,
+            "title": "M 2.2 - 10km ESE of Ojai, CA",
+            "mag": 2.16,
+            "place": "10km ESE of Ojai, CA",
+            "time": 1644571919000
+        },
+        {
+            "id": 2,
+            "title": "M 2.6 - 10km NW of Santa Paula, CA",
+            "mag": 2.68,
+            "place": "10km NW of Santa Paula, CA",
+            "time": 1644539746380
+        },
+        {
+            "id": 3,
+            "title": "M 4.4 - 10km ESE of Ojai, CA",
+            "mag": 4.16,
+            "place": "10km ESE of Ojai, CA",
+            "time": 1644571919030
+        }
+    ]
+};
