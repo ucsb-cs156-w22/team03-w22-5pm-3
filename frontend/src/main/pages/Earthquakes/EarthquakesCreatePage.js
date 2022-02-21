0ad6ffba8cad8caa2e2492d68557a531aca82c55
isaiahgarcia <isaiahgarcia@umail.ucsb.edu>
2022-02-20 16:55:43 -0800

    ig-earthquake retrieve nav tool implemented

--- frontend/src/main/pages/Earthquakes/EarthquakesCreatePage.js
+++ frontend/src/main/pages/Earthquakes/EarthquakesCreatePage.js
@@ -1,12 +1,16 @@
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import EarthquakesForm from "main/components/Earthquakes/EarthquakesForm";
+import { Navigate } from 'react-router-dom'
+import { useBackendMutation } from "main/utils/useBackend";
+import { toast } from "react-toastify";
 
 export default function EarthquakesCreatePage() {
   return (
     <BasicLayout>
       <div className="pt-2">
-        <h1>Earthquakes</h1>
+        <h1>Retrieve Earthquakes</h1>
         <p>
-          This is where the create page will go
+          This is where the retrieve page will go
         </p>
       </div>
     </BasicLayout>
